0fbbfeef8979ab1ff7b8e19b2e68340699eb0953
Jramongs18 <jramongs15@gmail.com>
2023-04-11 17:37:26 -0600

    Ejercicio 16 y Ejercicio 28

---
+++ Ejercicio1.js
@@ -0,0 +1,4 @@
+var miFuncion1 = function (parametro1, parametro2) {
+    console.log(Math.min(parametro1,parametro2))
+}
+miFuncion1(10,20);
---
+++ Ejercicio2.js
@@ -0,0 +1,7 @@
+var miFuncion2 = function (parametroa, parametrob, parametroc){
+    if ((parametroa == true || parametrob == true || parametroc == true)){
+        console.log("true");
+        
+    }else{console.log("false")}
+}
+miFuncion2(false, true, false)
